f578677db1fc7e5d9c245ffc557226e8d6af4089
Abhilash Korraprolu <42868745+Abhilash-Korraprolu@users.noreply.github.com>
2023-09-21 20:36:32 +0530

    Update with deployment code

--- streamlit_agent/search_and_chat.py
+++ streamlit_agent/search_and_chat.py
@@ -1,91 +1,105 @@
-import streamlit as st
-from langchain.agents import create_csv_agent, create_pandas_dataframe_agent
-from langchain.llms import OpenAI
+from langchain.agents import AgentType
+from langchain.agents import create_pandas_dataframe_agent
+from langchain.callbacks import StreamlitCallbackHandler
 from langchain.chat_models import ChatOpenAI
-from langchain.agents.agent_types import AgentType
-from langchain import PromptTemplate
+import streamlit as st
 import pandas as pd
+import os
 
-# Code----------------------------------------------------------------------
-
-# def create_agent():
-#     PAID_KEY_OPENAI = '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'
-#     data = ['shortlisted_datasets/sales 2yr data.csv']
-#     # data = []
-#     llm = ChatOpenAI(temperature=0, model="gpt-4", openai_api_key=PAID_KEY_OPENAI)
-#     agent = create_csv_agent(llm, data, verbose=True, agent_type=AgentType.OPENAI_FUNCTIONS)
-    
-#     return agent
-
-def create_agent(df):
-    PAID_KEY_OPENAI = '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'
-    llm = ChatOpenAI(temperature=0, model="gpt-4", openai_api_key=PAID_KEY_OPENAI)
-    agent = create_pandas_dataframe_agent(
-        llm, df, verbose=True, agent_type=AgentType.OPENAI_FUNCTIONS)
-    
-    return agent
+file_formats = {
+    "csv": pd.read_csv,
+    "xls": pd.read_excel,
+    "xlsx": pd.read_excel,
+    "xlsm": pd.read_excel,
+    "xlsb": pd.read_excel,
+}
 
 
-def make_api_call(prompt, df):
-    agent = create_agent(df)
-    ai_response = agent.run(prompt)
-    st.write(ai_response)
-    print(ai_response)
-    return ai_response
+def clear_submit():
+    """
+    Clear the Submit Button State
+    Returns:
+
+    """
+    st.session_state["submit"] = False
 
 
-def create_prompt(user_query):
-    prompt = PromptTemplate(input_variables=['user_query'], template=prompt_template)
-    prompt = prompt.format(user_query=user_query)
+@st.cache_data(ttl="2h")
+def load_data(uploaded_file):
+    try:
+        ext = os.path.splitext(uploaded_file.name)[1][1:].lower()
+    except:
+        ext = uploaded_file.split(".")[-1]
+    if ext in file_formats:
+        return file_formats[ext](uploaded_file)
+    else:
+        st.error(f"Unsupported file format: {ext}")
+        return None
 
-    return prompt
 
-
-# Work on causality
-# Still having black graphs
-prompt_template = """
-You are a Data Analyst at a Supermarket. Your job is to analyze data to answer your CFO's questions. These are the rules you must follow while answering:
-- Respond like you are reporting to your CFO on a streamlit app
-- Structure the response with headings
-- Additionally explain what factors in the data lead this answer. For example, if your answer is that the sales increased by 10 percent this month. You also explain what are the factors in the data that lead to this increase.
-- Suggest follow-up questions that the CFO could ask you and you could answer with the available data.
-- You must use at least one graph. The goal of a graph is to help the CFO understand your presentation easily
-- Use plotly when drawing a graph. Draw plots with blue shades. These blues should be darker than #caf0f8
-- Assume today's date is 1st Dec 2011
-
-CFO's Question: ### {user_query} ###
-"""
-
-prompt = PromptTemplate(input_variables=['user_query'], template=prompt_template)
-
-# UI-----------------------------------------------------------------------
+st.set_page_config(page_title="AI Data Analyst", page_icon="")
 # col1, col2, col3 = st.columns(3)
 # with col2:
 #     st.image(image='SSAFull2 copy.png')
 
-# Create a sidebar
-# st.sidebar.title('Upload your file')
 st.sidebar.header('Upload')
+uploaded_file = st.sidebar.file_uploader(
+    "Please upload the file you'd like to analyze.",
+    help='Supported formats: CSV, XLS, XLSX, XLSM, XLSB',
+    on_change=clear_submit,
+    )
 
-# Use the file_uploader function to allow the user to upload a file
-uploaded_file = st.sidebar.file_uploader("Please upload the file you'd like to analyze.", type="csv")
+if uploaded_file:
+    df = load_data(uploaded_file)
 
-# If a file is uploaded, read it into a pandas dataframe
-if uploaded_file is not None:
-    df = pd.read_csv(uploaded_file)
+# openai_api_key = st.sidebar.text_input("OpenAI API Key", type="password")
+openai_api_key = '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'
+if "messages" not in st.session_state or st.sidebar.button("Clear Chat"):
+    st.session_state["messages"] = [{"role": "assistant", "content": 'How can I help?'}]
 
-# st.markdown("<h1 style='text-align: center;'>AI CFO</h1>", unsafe_allow_html=True)
-    user_query = st.text_area(label='How can I help?')
-
-    if user_query:
-        st.write('Thinking...')
-        prompt = create_prompt(user_query=user_query)
-        ai_response = make_api_call(prompt, df)
-        st.write('------------')
-
-else:
-    # File is not uploaded, show a message
-    user_query = st.text_area(label='Please upload a file to get started.')
+for msg in st.session_state.messages:
+    st.chat_message(msg["role"]).write(msg["content"])
 
 
-# Exception handling: outputparserexception
+prompt_template = """
+You are a Data Analyst at a Supermarket. I'm your CFO. Your job is to analyze data to answer my questions. These are the rules you must follow while answering:
+- Respond like you are reporting on a streamlit app
+- Structure the response with headings
+- Additionally explain what factors in the data lead this answer. For example, if your answer is that the sales increased by 10 percent this month. You also explain what are the factors in the data that lead to this increase.
+- Suggest follow-up questions I could ask you and you could answer with the available data.
+- You must use at least one graph. The goal of a graph is to help me understand your presentation easily
+- Use plotly when drawing a graph. Draw plots with dark blue shades.
+- Assume today's date is 1st Dec 2011
+
+CFO's Question:
+"""
+
+# Draw plots with dark blue shades.
+
+if prompt := st.chat_input(placeholder="You question"): # Suggested questions chat hover
+    st.session_state.messages.append({"role": "user", "content": prompt_template + prompt})
+    st.chat_message("user").write(prompt) # User chat history after hitting enter.
+
+    llm = ChatOpenAI(temperature=0, model="gpt-4", openai_api_key=openai_api_key, streaming=True)
+    pandas_df_agent = create_pandas_dataframe_agent(
+        llm, df, verbose=True, agent_type=AgentType.OPENAI_FUNCTIONS,
+        handle_parsing_errors=True,
+    )
+
+    with st.chat_message("assistant", avatar="🧑‍💻"): 
+        st_cb = StreamlitCallbackHandler(st.container(), expand_new_thoughts=False)
+        # st.session_state.messages = st.session_state.messages.replace(prompt_template, '')
+        response = pandas_df_agent.run(st.session_state.messages, callbacks=[st_cb])
+
+        for monolog in st.session_state.messages:
+            if monolog['role'] == 'user':
+               monolog['content'] =  monolog['content'].replace(prompt_template, '')
+
+        st.session_state.messages.append({"role": "assistant", "content": response}) #Memory GPT response
+        st.write(response)
+        print('SESSION STATE MSG_____________________/n', st.session_state.messages)
+
+
+# Future Work
+
+# Chat Icons
